f9e903f59081399058f41d9acd8ebfe19e923c93
Wanderson Bueno <wanolibueno@gmail.com>
2025-03-03 17:36:39 -0300

    ajustes para deploy

---
+++ mobile/app.config.js
@@ -0,0 +1,71 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = {
+  name: "Prova de Vida",
+  slug: "prova-vida",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    bundleIdentifier: "com.provavida.app",
+    supportsTablet: true,
+    infoPlist: {
+      NSCameraUsageDescription: "Este aplicativo precisa de acesso à câmera para tirar fotos para prova de vida.",
+      NSPhotoLibraryUsageDescription: "Este aplicativo precisa de acesso às suas fotos para salvar as imagens da prova de vida.",
+      NSAppTransportSecurity: {
+        NSAllowsArbitraryLoads: true,
+        NSAllowsLocalNetworking: true
+      }
+    }
+  },
+  android: {
+    package: "com.provavida.app",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    permissions: [
+      "android.permission.INTERNET",
+      "android.permission.ACCESS_NETWORK_STATE",
+      "android.permission.CAMERA",
+      "android.permission.WRITE_EXTERNAL_STORAGE",
+      "android.permission.RECORD_AUDIO"
+    ],
+    softwareKeyboardLayoutMode: "resize",
+    compileSdkVersion: 33,
+    targetSdkVersion: 33,
+    minSdkVersion: 21
+  },
+  plugins: [
+    "expo-font",
+    [
+      "expo-image-picker",
+      {
+        photosPermission: "O aplicativo precisa de acesso à câmera para realizar a prova de vida."
+      }
+    ],
+    [
+      "expo-camera",
+      {
+        cameraPermission: "O aplicativo precisa de acesso à câmera para realizar a prova de vida."
+      }
+    ]
+  ],
+  experiments: {
+    tsconfigPaths: true
+  },
+  extra: {
+    eas: {
+      projectId: "6baf8832-3784-4d07-9f75-937761157167"
+    }
+  }
+};
