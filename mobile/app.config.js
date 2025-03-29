a03ff46e2fc8d76240762f35d81b9359f0042a6a
Wanderson Bueno <wanolibueno@gmail.com>
2025-03-29 01:07:16 -0300

    commit antes de mexer novamnete

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,26 +1,26 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = {
-  name: "E-Prev",
-  slug: "prova-vida",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  userInterfaceStyle: "light",
+  name: 'E-Prev',
+  slug: 'prova-vida',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
   splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#0284C7"
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#0284C7'
   },
-  assetBundlePatterns: [
-    "**/*"
-  ],
+  assetBundlePatterns: ['**/*'],
   ios: {
-    bundleIdentifier: "com.provavida.app",
+    bundleIdentifier: 'com.provavida.app',
     supportsTablet: true,
     infoPlist: {
-      NSCameraUsageDescription: "Este aplicativo precisa de acesso à câmera para tirar fotos para prova de vida.",
-      NSPhotoLibraryUsageDescription: "Este aplicativo precisa de acesso às suas fotos para salvar as imagens da prova de vida.",
+      NSCameraUsageDescription:
+        'Este aplicativo precisa de acesso à câmera para tirar fotos para prova de vida.',
+      NSPhotoLibraryUsageDescription:
+        'Este aplicativo precisa de acesso às suas fotos para salvar as imagens da prova de vida.',
       NSAppTransportSecurity: {
         NSAllowsArbitraryLoads: true,
         NSAllowsLocalNetworking: true
@@ -28,42 +28,44 @@ module.exports = {
     }
   },
   android: {
-    package: "com.provavida.app",
+    package: 'com.provavida.app',
     versionCode: 1,
     adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#ffffff"
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff'
     },
     permissions: [
-      "android.permission.INTERNET",
-      "android.permission.ACCESS_NETWORK_STATE",
-      "android.permission.CAMERA",
-      "android.permission.WRITE_EXTERNAL_STORAGE",
-      "android.permission.READ_EXTERNAL_STORAGE"
+      'android.permission.INTERNET',
+      'android.permission.ACCESS_NETWORK_STATE',
+      'android.permission.CAMERA',
+      'android.permission.WRITE_EXTERNAL_STORAGE',
+      'android.permission.READ_EXTERNAL_STORAGE'
     ],
-    softwareKeyboardLayoutMode: "resize",
+    softwareKeyboardLayoutMode: 'resize',
     compileSdkVersion: 33,
     targetSdkVersion: 33,
     minSdkVersion: 21
   },
   plugins: [
-    "expo-font",
+    'expo-font',
     [
-      "expo-image-picker",
+      'expo-image-picker',
       {
-        photosPermission: "O aplicativo precisa de acesso à câmera para realizar a prova de vida."
+        photosPermission:
+          'O aplicativo precisa de acesso à câmera para realizar a prova de vida.'
       }
     ],
     [
-      "expo-camera",
+      'expo-camera',
       {
-        cameraPermission: "O aplicativo precisa de acesso à câmera para realizar a prova de vida."
+        cameraPermission:
+          'O aplicativo precisa de acesso à câmera para realizar a prova de vida.'
       }
     ]
   ],
   extra: {
     eas: {
-      projectId: "6baf8832-3784-4d07-9f75-937761157167"
+      projectId: '6baf8832-3784-4d07-9f75-937761157167'
     }
   }
-};
+}
